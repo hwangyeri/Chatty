b0ad3771ab408c70e259589f7a1788492529641c
hwangyeri <dpfl420@naver.com>
2024-01-06 02:56:34 +0900

    [Remove] AuthViewModel: kakaoLoginButtonTap 제거 및 주석 내용 추가

--- Chatty/Chatty/Source/Domain/View/Auth/AuthViewModel.swift
+++ Chatty/Chatty/Source/Domain/View/Auth/AuthViewModel.swift
@@ -12,37 +12,24 @@ import RxCocoa
 final class AuthViewModel: BaseViewModel {
     
     struct Input {
-        let appleLoginButton: ControlEvent<Void>
-        let kakaoLoginButton: ControlEvent<Void>
-        let emailLoginButton: ControlEvent<Void>
-        let joinTextButton: ControlEvent<Void>
+        let appleLoginButton: ControlEvent<Void> // 애플 로그인 버튼
+        let emailLoginButton: ControlEvent<Void> // 이메일 로그인 버튼
+        let joinTextButton: ControlEvent<Void> // 회원가입 버튼
     }
     
     struct Output {
         let appleLoginButtonTap: Driver<Void>
-//        let kakaoLoginButtonTap: Driver<Void>
         let emailLoginButtonTap: Driver<Void>
         let joinTextButtonTap: Driver<Void>
     }
     
 //    private let disposeBag = DisposeBag()
     
-    private let kakaoLoginViewModel = SocialLoginViewModel()
-    
     func transform(input: Input) -> Output {
         
         let appleLoginButtonTap = input.appleLoginButton
             .throttle(.seconds(1), scheduler: MainScheduler.instance)
             .asDriver(onErrorJustReturn: ())
-        
-        let kakaoLoginButtonTap = input.kakaoLoginButton
-            .throttle(.seconds(1), scheduler: MainScheduler.instance)
-            .subscribe(with: self) { owner, _ in
-                // 1. 카카오톡 소셜 로그인
-                owner.kakaoLoginViewModel.loginWithKakaoTalk()
-                
-                // 2. 카카오톡 로그인 정보 서버로 보내기
-            }
         
         let emailLoginButtonTap = input.emailLoginButton
             .throttle(.seconds(1), scheduler: MainScheduler.instance)
@@ -55,7 +42,6 @@ final class AuthViewModel: BaseViewModel {
         
         return Output(
             appleLoginButtonTap: appleLoginButtonTap,
-//            kakaoLoginButtonTap: kakaoLoginButtonTap,
             emailLoginButtonTap: emailLoginButtonTap,
             joinTextButtonTap: joinTextButtonTap
         )
