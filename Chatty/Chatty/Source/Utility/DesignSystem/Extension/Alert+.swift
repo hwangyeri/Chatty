0ce8a4b9f166c7702020be6424d85f12b2c50035
hwangyeri <dpfl420@naver.com>
2024-01-05 16:00:20 +0900

    [Feat] Alert+ 생성

---
+++ Chatty/Chatty/Source/Utility/DesignSystem/Extension/Alert+.swift
@@ -0,0 +1,46 @@
+//
+//  Alert+.swift
+//  Chatty
+//
+//  Created by Yeri Hwang on 2024/01/05.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    // 확인 얼럿
+    func showOkAlert(title: String, message: String, handler: (() -> ())? = nil ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .default) { _ in
+            handler?()
+        }
+        
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+    
+    // 토스트
+    func showToast(message : String) {
+        let toastLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 175, height: 36))
+        toastLabel.center = CGPoint(x: self.view.frame.size.width / 2, y: self.view.frame.size.height / 2)
+        toastLabel.backgroundColor = UIColor.point
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = .customFont(.body)
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 8
+        toastLabel.clipsToBounds  =  true
+        
+        self.view.addSubview(toastLabel)
+        
+        UIView.animate(withDuration: 2.5, delay: 0.2, options: .curveLinear, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: { (isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+    
+}
+
