1a513d922116718149f1ab48bd3764145a6ead5b
hwangyeri <dpfl420@naver.com>
2024-01-06 02:47:12 +0900

    [Feat] Constants 파일 생성

---
+++ Chatty/Chatty/Source/Utility/DesignSystem/Constant/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Chatty
+//
+//  Created by Yeri Hwang on 2024/01/05.
+//
+
+import Foundation
+
+enum Constants {
+    
+    // API
+    static let sesacKey = "SesacKey"
+    static let contentType = "Content-Type"
+    static let applicationJSON = "application/json"
+    
+}
