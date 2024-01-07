f2aabde8ce9b1a6851fcb2b5a8ea5babc6d47fdf
hwangyeri <dpfl420@naver.com>
2024-01-07 20:34:27 +0900

    [Feat] 런치스크린 1초간 띄우기

--- Chatty/Chatty/Application/AppDelegate.swift
+++ Chatty/Chatty/Application/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
+        Thread.sleep(forTimeInterval: 1.0)
+        
         // 메인 번들에 있는 카카오 앱 키 불러오기
         let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
         
