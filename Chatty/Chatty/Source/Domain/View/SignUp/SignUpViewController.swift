e562c16e1a2d4aadf4a91765f83e2a46ddb76912
hwangyeri <dpfl420@naver.com>
2024-01-09 19:11:37 +0900

    [Feat] RxSwift, MVVM, Input-Output 패턴 적용해 회원가입 VC 구현
    - 중복 확인 버튼을 통해 이메일 유효성 검사, 사용 가능 여부 처리 및 토스트 메세지 기능 구현
    - 가입하기 버튼을 통해 닉네임/전화번호/비밀번호/비밀번호 확인 유효성 검사 로직 및 UI 업데이트 기능 구현

--- Chatty/Chatty/Source/Domain/View/SignUp/SignUpViewController.swift
+++ Chatty/Chatty/Source/Domain/View/SignUp/SignUpViewController.swift
@@ -6,10 +6,19 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class SignUpViewController: BaseViewController {
     
     private let mainView = SignUpView()
+    
+    private let viewModel = SignUpViewModel()
+    
+    private let disposeBag = DisposeBag()
+    
+    // 중복 확인, 사용 가능한 이메일 저장소
+    private let isCheckedEmailStore = BehaviorRelay<String>(value: "")
     
     override func loadView() {
         self.view = mainView
@@ -18,7 +27,105 @@ final class SignUpViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        bind()
+    }
+    
+    override func configureLayout() {
+        self.view.keyboardLayoutGuide.followsUndockedKeyboard = true
+        mainView.checkEmailButton.addTarget(self, action: #selector(checkEmailButtonTap), for: .touchUpInside)
+    }
+    
+    // 이메일 중복 확인 버튼
+    @objc private func checkEmailButtonTap() {
+        guard let email = mainView.emailTextField.text else {
+            print("emailTextField.text Error")
+            return
+        }
+        
+        let isValid = isEmailValid(email)
+        
+        if isValid {
+            NetworkManager.shared.requestCheckEmail(router: .usersValidationEmail(email: email)) { [weak self] result in
+                switch result {
+                case .success(let data):
+                    print("****", data)
+                    self?.showToast(message: "사용 가능한 이메일입니다.")
+                    self?.isCheckedEmailStore.accept(email)
+                case .failure(let error):
+                    print("****", error.errorDescription)
+                    self?.showToast(message: "이미 사용 중인 이메일입니다.")
+                }
+            }
+        } else {
+            showToast(message: "이메일 형식이 올바르지 않습니다.")
+        }
+    }
+    
+    // 이메일 유효성 검증
+    private func isEmailValid(_ email: String) -> Bool {
+        let emailRegex = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
+    }
+    
+    private func bind() {
+        
+        let input = SignUpViewModel.Input(
+            xButton: mainView.xButton.rx.tap,
+            isCheckedEmailStore: isCheckedEmailStore,
+            emailTextField: mainView.emailTextField.rx.text.orEmpty,
+            checkEmailButton: mainView.checkEmailButton.rx.tap,
+            nicknameTextField: mainView.nicknameTextField.rx.text.orEmpty,
+            contactTextField: mainView.contactTextField.rx.text.orEmpty,
+            passwordTextField: mainView.passwordTextField.rx.text.orEmpty,
+            checkPasswordTextField: mainView.checkPasswordTextField.rx.text.orEmpty,
+            signUpButton: mainView.signUpButton.rx.tap
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        // 나가기 버튼
+        output.xButtonTap
+            .drive(with: self) { owner, _ in
+                owner.dismiss(animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        // 중복 확인 버튼 활성화
+        output.isNotEmpty
+            .drive(with: self) { owner, isValid in
+                owner.mainView.checkEmailButton.backgroundColor = isValid ? .point : .inactive
+                owner.mainView.checkEmailButton.isEnabled = isValid
+            }
+            .disposed(by: disposeBag)
+        
+        // 가입하기 버튼 활성화
+        output.isSignUpButtonValid
+            .drive(with: self) { owner, isValid in
+                owner.mainView.signUpButton.backgroundColor = isValid ? .point : .inactive
+                owner.mainView.signUpButton.isEnabled = isValid
+            }
+            .disposed(by: disposeBag)
+        
+        // 가입하기 유효성 검증 결과 UI 반영
+        output.isSignUpValid
+            .bind(with: self) { owner, isValid in
+                print("가입하기 버튼 클릭")
+                
+                let labels = [
+                    owner.mainView.emailLabel,
+                    owner.mainView.nicknameLabel,
+                    owner.mainView.contactLabel,
+                    owner.mainView.passwordLabel,
+                    owner.mainView.checkPasswordLabel
+                ]
+                
+                zip(labels, isValid).forEach { label, isValid in
+                    label.textColor = isValid ? .textPrimary : .error
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
     
 }
+
