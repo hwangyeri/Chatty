ffa0a811bf6ef7ca1af28a80c23f6901a465e23d
hwangyeri <dpfl420@naver.com>
2024-01-12 20:47:00 +0900

    [Feat] AddVC 카메라/앨범 기능, 버튼 이벤트 처리 및 UI 업데이트 구현

--- Chatty/Chatty/Source/Domain/View/Workspace/Add/AddViewController.swift
+++ Chatty/Chatty/Source/Domain/View/Workspace/Add/AddViewController.swift
@@ -6,24 +6,175 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import PhotosUI
 
-class AddViewController: UIViewController {
+final class AddViewController: BaseViewController {
 
+    private let mainView = AddView()
+    
+    private let viewModel = AddViewModel()
+    
+    private let disposeBag = DisposeBag()
+    
+    private let imageData = PublishRelay<Data>()
+    
+    override func loadView() {
+        self.view = mainView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        bind()
+    }
+    
+    // UIImage -> Data 객체로 변환하는 메서드
+    private func convertImageToData(_ image: UIImage) -> Data {
+        return image.jpegData(compressionQuality: 0.2) ?? Data()
+    }
+    
+    private func bind() {
+        
+        let input = AddViewModel.Input(
+            xButton: mainView.xButton.rx.tap, 
+            profileImageButton: mainView.workspaceImageButton.rx.tap, 
+            nameTextField: mainView.nameTextField.rx.text.orEmpty,
+            explainTextField: mainView.explainTextField.rx.text.orEmpty,
+            workspaceImage: imageData,
+            doneButton: mainView.doneButton.rx.tap
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        output.xButtonTap
+            .drive(with: self) { owner, _ in
+                let vc = HomeViewController()
+                let rootVC = UINavigationController(rootViewController: vc)
+                ChangeRootVCManager.shared.changeRootVC(rootVC)
+            }
+            .disposed(by: disposeBag)
+        
+        // 프로필 버튼 탭
+        output.profileImageButtonTap
+            .drive(with: self) { owner, _ in
+                let actionSheet = UIAlertController(title: "워크스페이스 이미지를 넣어주세요! (필수)", message: nil, preferredStyle: .actionSheet)
+                
+                let cameraAction = UIAlertAction(title: "카메라", style: .default) { _ in
+                    print("카메라 액션시트 클릭")
+                    let imagePicker = UIImagePickerController()
+                    
+                    imagePicker.sourceType = .camera
+                    imagePicker.delegate = self
+                    
+                    owner.present(imagePicker, animated: true)
+                }
+                
+                let albumAction = UIAlertAction(title: "앨범", style: .default) { _ in
+                    print("앨범 액션시트 클릭")
+                    var configuration = PHPickerConfiguration()
+                    configuration.selectionLimit = 1
+                    configuration.filter = .images
+                    
+                    let phPicker = PHPickerViewController(configuration: configuration)
+                    phPicker.delegate = self
+                    
+                    owner.present(phPicker, animated: true)
+                }
+                
+                let cancelAction = UIAlertAction(title: "취소", style: .destructive)
+                
+                actionSheet.addAction(cameraAction)
+                actionSheet.addAction(albumAction)
+                actionSheet.addAction(cancelAction)
+                
+                owner.present(actionSheet, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        // 생성하기 버튼 활성화
+        output.doneButtonValid
+            .drive(with: self) { owner, isValid in
+                owner.mainView.doneButton.backgroundColor = isValid ? .point : .inactive
+                owner.mainView.doneButton.isEnabled = isValid
+            }
+            .disposed(by: disposeBag)
+        
+        // 이름 유효성 검증 결과 UI 업데이트
+        output.isNameValidResult
+            .bind(with: self) { owner, isValid in
+                print("+++", isValid)
+                owner.mainView.nameLabel.textColor = isValid ? .textPrimary : .error
+            }
+            .disposed(by: disposeBag)
+        
+        // 워크스페이스 생성 API 결과 UI 업데이트
+        output.isDoneButtonValid
+            .bind(with: self) { owner, isValid in
+                if isValid {
+                    // 루트뷰 전환
+                    let vc = HomeViewController()
+                    let rootVC = UINavigationController(rootViewController: vc)
+                    ChangeRootVCManager.shared.changeRootVC(rootVC)
+                } else {
+                    let buttonTopY = owner.mainView.doneButton.frame.origin.y
+                    owner.showToast(message: "에러가 발생했어요.\n잠시 후 다시 시도해주세요.", y: buttonTopY)
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// UIImagePicker 카메라
+extension AddViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+   
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            let imageToData = convertImageToData(image)
+            mainView.workspaceImageButton.setImage(UIImage(data: imageToData), for: .normal)
+            imageData.accept(imageToData)
+        } else {
+            print("Error converting image to data.")
+        }
+        
+        dismiss(animated: true)
     }
-    */
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    
+}
 
+// PHPicker 앨범
+extension AddViewController: PHPickerViewControllerDelegate {
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        for result in results {
+            let itemProvider = result.itemProvider
+            
+            if itemProvider.canLoadObject(ofClass: UIImage.self) {
+                itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                    DispatchQueue.main.async {
+                        if let selectedImage = image as? UIImage {
+                            if let imageToData = self?.convertImageToData(selectedImage) {
+                                self?.mainView.workspaceImageButton.setImage(UIImage(data: imageToData), for: .normal)
+                                self?.imageData.accept(imageToData)
+                            } else {
+                                print("Error converting image to data.")
+                            }
+                        } else {
+                            print("Error loading image.")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 }
