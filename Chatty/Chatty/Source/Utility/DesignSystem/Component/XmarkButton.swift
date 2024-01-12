66d8ecb90ac79fe563a6f57f968644c725b3c606
hwangyeri <dpfl420@naver.com>
2024-01-12 10:49:07 +0900

    [Rename] XmarkButton로 이름 변경

---
+++ Chatty/Chatty/Source/Utility/DesignSystem/Component/XmarkButton.swift
@@ -0,0 +1,29 @@
+//
+//  CSymbolButton.swift
+//  Chatty
+//
+//  Created by Yeri Hwang on 2024/01/06.
+//
+
+import UIKit
+
+final class XmarkButton: UIButton {
+    
+    init() {
+        super.init(frame: .zero)
+        
+        // default == X Button
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 16, weight: .regular)
+        let image = UIImage(systemName: "xmark", withConfiguration: imageConfig)
+        
+        self.setImage(image, for: .normal)
+        self.tintColor = .black
+        self.contentMode = .scaleAspectFit
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
