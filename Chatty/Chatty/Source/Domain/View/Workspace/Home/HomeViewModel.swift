4276a44ce01c10ea26a288ed4e3c2832764019d7
hwangyeri <dpfl420@naver.com>
2024-01-14 23:23:14 +0900

    [Feat] HomeViewModel 버튼 이벤트 처리 로직 구현

--- Chatty/Chatty/Source/Domain/View/Workspace/Home/HomeViewModel.swift
+++ Chatty/Chatty/Source/Domain/View/Workspace/Home/HomeViewModel.swift
@@ -12,22 +12,49 @@ import RxCocoa
 final class HomeViewModel: BaseViewModel {
     
     struct Input {
-        let wsNameButton: ControlEvent<Void>
-        let myProfileButton: ControlEvent<Void>
-        let createButton: ControlEvent<Void>
-        let postButton: ControlEvent<Void>
+        let wsNameButton: ControlEvent<Void> // 워크스페이스 이름 버튼
+        let myProfileButton: ControlEvent<Void> // 내 프로필 버튼
+        let createButton: ControlEvent<Void> // 워크스페이스 생성 버튼
+        let postButton: ControlEvent<Void> // 작성하기 버튼
     }
     
     struct Output {
-       
+        let wsNameButtonTap: Driver<Void>
+        let myProfileButtonTap: Driver<Void>
+        let createButtonTap: Driver<Void>
+        let postButtonTap: Driver<Void>
     }
     
     private let disposeBag = DisposeBag()
     
     func transform(input: Input) -> Output {
         
+        // 워크스페이스 이름 탭 -> SideMenu
+        let wsNameButtonTap = input.wsNameButton
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: ())
+        
+        // 내 프로필 버튼 탭
+        let myProfileButtonTap = input.myProfileButton
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: ())
+        
+        // 워크스페이스 생성하기 버튼 탭
+        let createButtonTap = input.createButton
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: ())
+        
+        // 작성하기 버튼 텝
+        let postButtonTap = input.createButton
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: ())
+        
         
         return Output(
+            wsNameButtonTap: wsNameButtonTap, 
+            myProfileButtonTap: myProfileButtonTap, 
+            createButtonTap: createButtonTap, 
+            postButtonTap: postButtonTap
         )
     }
     
