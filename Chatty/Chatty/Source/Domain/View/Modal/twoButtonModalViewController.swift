0def864f30b63110fa3904abc94dd4ffbf91f655
hwangyeri <dpfl420@naver.com>
2024-01-24 20:02:46 +0900

    [Feat] twoButtonModalVC: ModalAction 열거형, setLabelText 메서드를 활용해 재사용뷰 분기처리

--- Chatty/Chatty/Source/Domain/View/Modal/twoButtonModalViewController.swift
+++ Chatty/Chatty/Source/Domain/View/Modal/twoButtonModalViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+enum ModalAction {
+    case channelJoin // 채널 참여
+}
+
 final class twoButtonModalViewController: BaseViewController {
     
+    var modalAction: ModalAction?
+    
     var workspaceID: Int?
+    
+    var channelName: String?
     
     private let mainView = twoButtonModalView()
     
@@ -27,11 +35,25 @@ final class twoButtonModalViewController: BaseViewController {
         super.viewDidLoad()
 
         viewModel.workspaceID = workspaceID
+        setLabelText()
         bind()
     }
     
     override func configureLayout() {
         view.backgroundColor = .alpha
+    }
+    
+    private func setLabelText() {
+        switch modalAction {
+        case .channelJoin:
+            mainView.mainLabel.text = "채널 참여"
+            mainView.subLabel.text = "[\(channelName)] 채널에 참여하시겠습니까?"
+            mainView.deleteButton.setTitle("확인", for: .normal)
+        default:
+            mainView.mainLabel.text = "워크스페이스 삭제"
+            mainView.subLabel.text = "정말 이 워크스페이스를 삭제하시겠습니까? 삭제 시 채널/멤버/채팅 등 워크스페이스 내의 모든 정보가 삭제되며 복구할 수 없습니다."
+            mainView.deleteButton.setTitle("취소", for: .normal)
+        }
     }
     
     private func bind() {
