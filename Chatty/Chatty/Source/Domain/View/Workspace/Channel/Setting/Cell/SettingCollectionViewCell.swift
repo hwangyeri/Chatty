a8b97ec8d08b1e4533f059b0d0901b48e84377a6
hwangyeri <dpfl420@naver.com>
2024-01-25 23:14:50 +0900

    [Style] SettingCollectionViewCell UI 구현

---
+++ Chatty/Chatty/Source/Domain/View/Workspace/Channel/Setting/Cell/SettingCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  SettingCollectionViewCell.swift
+//  Chatty
+//
+//  Created by Yeri Hwang on 2024/01/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class SettingCollectionViewCell: BaseCollectionViewCell {
+    
+    let imgView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.layer.cornerRadius = 8
+        $0.clipsToBounds = true
+        $0.image = .dummy
+    }
+    
+    let nameLabel = CLabel(text: "Hue", custFont: .body).then {
+        $0.textAlignment = .center
+    }
+    
+    override func configureHierarchy() {
+        [imgView, nameLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    override func configureLayout() {
+        imgView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(8)
+            make.horizontalEdges.equalToSuperview().inset(12)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(imgView.snp.bottom).offset(4)
+            make.horizontalEdges.equalToSuperview().inset(8)
+            make.bottom.equalToSuperview().inset(8)
+        }
+    }
+    
+}
