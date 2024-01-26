74ee4cdb04cf07bfdf27cf38356134ed9b61eacd
hwangyeri <dpfl420@naver.com>
2024-01-26 15:46:35 +0900

    [Feat] ChattingImageCollectionViewCell 생성

---
+++ Chatty/Chatty/Source/Domain/View/Workspace/Channel/Chatting/Cell/ChattingImageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChattingImageCollectionViewCell.swift
+//  Chatty
+//
+//  Created by Yeri Hwang on 2024/01/25.
+//
+
+import UIKit
+
+final class ChattingImageCollectionViewCell: BaseCollectionViewCell {
+    
+}
