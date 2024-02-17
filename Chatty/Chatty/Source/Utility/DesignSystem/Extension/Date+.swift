002c0cd88ed65819282c4ab12834900fce64899a
hwangyeri <dpfl420@naver.com>
2024-02-17 23:20:32 +0900

    [Refactor] Date+: createdAt 타입 변경으로 인한 DTFormatter 메서드 수정

--- Chatty/Chatty/Source/Utility/DesignSystem/Extension/Date+.swift
+++ Chatty/Chatty/Source/Utility/DesignSystem/Extension/Date+.swift
@@ -9,20 +9,12 @@ import Foundation
 
 extension Date {
     
-    static func DTFormatter(_ dateString: String) -> String? {
-        let isoFormatter = ISO8601DateFormatter()
-        isoFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+    static func DTFormatter(_ date: Date) -> String? {
+        let myDateFormatter = DateFormatter()
+        myDateFormatter.dateFormat = "h:mm a"
+        myDateFormatter.timeZone = TimeZone(identifier: "UTC")
         
-        if let date = isoFormatter.date(from: dateString) {
-            let myDateFormatter = DateFormatter()
-            myDateFormatter.dateFormat = "h:mm a"
-            myDateFormatter.timeZone = TimeZone(identifier: "UTC") 
-            
-            return myDateFormatter.string(from: date)
-        }
-        
-        return nil
+        return myDateFormatter.string(from: date)
     }
-    
     
 }
