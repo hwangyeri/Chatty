11a798880381aa0c360b9889739574120275144f
hwangyeri <dpfl420@naver.com>
2024-02-27 21:49:17 +0900

    [Refactor] BaseTableViewCell selectionStyle 추가

--- Chatty/Chatty/Source/Domain/Base/BaseTableViewCell.swift
+++ Chatty/Chatty/Source/Domain/Base/BaseTableViewCell.swift
@@ -11,8 +11,11 @@ class BaseTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
         configureHierarchy()
         configureLayout()
+        
+        self.selectionStyle = .none
     }
     
     required init?(coder: NSCoder) {
